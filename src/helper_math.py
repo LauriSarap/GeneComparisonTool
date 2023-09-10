0ca28c5dcb5e02ebf64745fb4e6ec0f24805c098
Lauri Sarap <lauri.aardla@gmail.com>
2023-09-10 18:14:05 +0300

    Code updates. More modular

--- src/helper_math.py
+++ src/helper_math.py
@@ -1,5 +1,10 @@
-from Bio import pairwise2
+from Bio import pairwise2, SeqIO
 from Bio.pairwise2 import format_alignment
+from collections import defaultdict
+import csv
+import pandas as pd
+import os
+import re
 
 
 def calculate_similarity(seq1, seq2):
@@ -12,13 +17,91 @@ def calculate_similarity(seq1, seq2):
     return (score / max_length) * 100, best_alignment
 
 
-def calculate_similarity_with_print(seq1, seq2):
-    # Assuming both sequences are of the same length
+def calculate_results(fasta_files, gene_name):
 
-    print("Homo sapiens sequence:")
-    print(seq1)
-    print("Other sequence:")
-    print(seq2)
+    results = defaultdict(dict)
 
-    matches = sum(1 for a, b in zip(seq1, seq2) if a == b)
-    return (matches / len(seq1)) * 100
+    # Perform alignment
+    print(f'Calculating results for {gene_name}...')
+    for fasta_file in fasta_files:
+        variant_name = fasta_file.split("_")[1]
+        print("Processing file: " + variant_name + ".")
+        records = list(SeqIO.parse(fasta_file, "fasta"))
+
+        homo_sapiens_seq = None
+        for record in records:
+            if re.match(r'^ENS[T|P|G|E|FM|R]\d+$', record.id):  # Homo sapiens
+                homo_sapiens_seq = record.seq
+                break
+
+        if homo_sapiens_seq is not None:
+            for record in records:
+                if not re.match(r'^ENS[T|P|G|E|FM|R]\d+$', record.id):  # Exclude Homo sapiens
+                    species = record.id
+                    similarity, best_alignment = calculate_similarity(homo_sapiens_seq, record.seq)
+                    results[variant_name][species] = {
+                        'similarity': similarity,
+                        'score': best_alignment[2],
+                        'start': best_alignment[3],
+                        'end': best_alignment[4]
+                    }
+        else:
+            print(f"No Homo sapiens sequence found in {fasta_file}. Skipping.")
+
+    delete_csv_file(f'results/{gene_name}_alignment_results.csv')
+
+    # Map animal names to species prefixes
+    species_mapping = {}
+    with open("data/species_prefixes.txt", "r") as f:
+        next(f)  # Skip the header
+        for line in f:
+            prefix, name = line.strip().split("\t")
+            species_mapping[prefix] = name
+
+    # Save alignment scores to .csv
+    print(f'Saving {gene_name} results to .csv..')
+    with open(f"results/{gene_name}_alignment_results.csv", 'w', newline='') as csvfile:
+        fieldnames = ['Variant Name', 'Species', 'Similarity', 'Score', 'Start', 'End']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for variant_name, species_data in results.items():
+            for species, data in species_data.items():
+                species_prefix = max((k for k in species_mapping.keys() if species.startswith(k)), key=len, default=None)
+                species_name = species_mapping.get(species_prefix, species)
+                writer.writerow({
+                    'Variant Name': variant_name,
+                    'Species': species_name,
+                    'Similarity': data['similarity'],
+                    'Score': data['score'],
+                    'Start': data['start'],
+                    'End': data['end']
+                })
+    print(f'Finished saving {gene_name} results to .csv!')
+
+
+def perform_analysis(gene_name):
+    delete_csv_file(f"results_analysis/{gene_name}_aggregated_similarity.csv")
+    print(f'Performing analysis for {gene_name}..')
+
+    df = pd.read_csv(f"results/{gene_name}_alignment_results.csv")
+    grouped_by_species_df = df.groupby('Species').agg({'Similarity': 'sum'}).reset_index()
+    grouped_by_species_df_sorted = grouped_by_species_df.sort_values(by='Similarity', ascending=False)
+    grouped_by_species_df_sorted.to_csv(f"results_analysis/{gene_name}_aggregated_similarity", index=False)
+
+    print(f'Finished performing analysis for {gene_name}!')
+
+
+def delete_all_csv_files(directory):
+    print(f'Deleting all .csv files in {directory}..')
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            os.remove(os.path.join(directory, filename))
+
+
+def delete_csv_file(filename):
+    print(f'Deleting {filename} if exists..')
+    if os.path.exists(filename):
+        os.remove(filename)
+
+
--- src/main.py
+++ src/main.py
@@ -1,62 +1,27 @@
-from Bio import SeqIO
 from collections import defaultdict
-from helper_math import calculate_similarity, calculate_similarity_with_print
-import csv
-
+from helper_math import calculate_results
+from helper_math import perform_analysis
 
 # SETTINGS
-UNALIGNED_DATA = True
+EVALUATE_RNA5S = False
+EVALUATE_HOXA = True
 
-
-results = defaultdict(dict)
-
-fasta_files = []
-
-if UNALIGNED_DATA:
+# MAIN
+if EVALUATE_RNA5S:
+    RNA5S_fasta_files = []
     for i in range(1, 18):
         if i != 8:
-            fasta_files.insert(i, f"data/Human_RNA5S{i}_orthologues.fa")
+            RNA5S_fasta_files.insert(i, f"data/RNA5S/Human_RNA5S{i}_orthologues.fa")
 
-    for fasta_file in fasta_files:
-        variant_name = fasta_file.split("_")[1]
-        records = list(SeqIO.parse(fasta_file, "fasta"))
+    calculate_results(RNA5S_fasta_files, "RNA5S")
+    perform_analysis("RNA5S")
 
-        homo_sapiens_seq = None
-        for record in records:
-            if "ENST" in record.id:
-                homo_sapiens_seq = record.seq
-                break
+if EVALUATE_HOXA:
+    HOXA_fasta_files = []
+    for i in range(1, 14):
+        if i != 8 and i != 11:
+            HOXA_fasta_files.insert(i, f"data/HOXA/Human_HOXA{i}_orthologues.fa")
 
-        for record in records:
-            if "ENST" not in record.id:
-                species = record.id
-                similarity, best_alignment = calculate_similarity(homo_sapiens_seq, record.seq)
-                results[variant_name][species] = {
-                    'similarity': similarity,
-                    'score': best_alignment[2],
-                    'start': best_alignment[3],
-                    'end': best_alignment[4]
-                }
-
-    print("Results by variant and species!")
-    for variant_name, species_data in results.items():
-        print(f"Variant: {variant_name}")
-        for species, similarity in species_data.items():
-            print(f"  Species: {species}, Similarity: {similarity}")
-
-    with open('alignment_results.csv', 'w', newline='') as csvfile:
-        fieldnames = ['Variant Name', 'Species', 'Similarity', 'Score', 'Start', 'End']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for variant_name, species_data in results.items():
-            for species, data in species_data.items():
-                writer.writerow({
-                    'Variant Name': variant_name,
-                    'Species': species,
-                    'Similarity': data['similarity'],
-                    'Score': data['score'],
-                    'Start': data['start'],
-                    'End': data['end']
-                })
+    calculate_results(HOXA_fasta_files, "HOXA")
+    perform_analysis("HOXA")
 
